9da9b9c8fd607b59cfb131dde73beb5b5830f61d
zach kadish <zachkadish@gmail.com>
2023-10-14 04:53:36 -0700

    initial commit

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ## resources
 
 - Main tutorial: https://blog.logrocket.com/zero-downtime-deploys-with-digitalocean-github-and-docker/
-- Set up ssh: https://gist.github.com/danieltorscho/c104c23e97c840f09fd9a186ec092b28
+- Set up ssh in the droplets: https://gist.github.com/danieltorscho/c104c23e97c840f09fd9a186ec092b28
+- better docker file for node: https://devpress.csdn.net/cicd/62ec1cc319c509286f416470.htmldocker build -t my-app:dev .
 - 
--- api.mjs
+++ api.mjs
@@ -1,16 +1,29 @@
 #!/usr/bin/env node
 
-import fastify from 'fastify';
-const server = fastify();
+import Fastify from 'fastify';
+const fastify = Fastify({
+  logger: true
+});
 let die = false;
 const id = Math.floor(Math.random()*1000);
 
-server.get('/', async () => ({ api: 'happy response', id }));
+fastify.get('/', async () => ({ api: 'happy response', id }));
 
-server.get('/health', async (_req, reply) => {
+fastify.get('/health', async (_req, reply) => {
   if (die) {
     reply.code(503).send({ status: 'shutdown' });
   } else {
     reply.code(200).send({ status: 'ok' });
   }
 });
+
+// Run the server!
+fastify.get('/shutdown', async () => {
+  die = true;
+  return { shutdown: true };
+});
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) throw err
+  console.log(`Server is now listening on ${address}`);
+})
